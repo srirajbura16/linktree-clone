505e8258469a2cedbfdeea2b44ade7590abc7ad3
Sriraj Bura <sriraj.bura16@gmail.com>
2022-02-06 20:35:40 -0500

    added static bio page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,58 +13,35 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className={styles.container}>
+      <div className={styles.profile_pic}>
+        <Image
+          src="/images/me.jpg"
+          alt="Picture of Sriraj Bura"
+          className={styles.img}
+          width={250}
+          height={250}
+        />
+      </div>
+      <div className={styles.links}>
+        <div className={styles.link}>
+          <Link href="https://twitter.com/SrirajBura">
+            <a>Twitter</a>
+          </Link>
         </div>
+        <div className={styles.link}>
+          <Link href="https://github.com/srirajbura16">
+            <a>Github</a>
+          </Link>
+        </div>
+        <div className={styles.link}>
+          <Link href="https://www.linkedin.com/in/srirajbura/">
+            <a>Linkedin</a>
+          </Link>
+        </div>
+      </div>
+    </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
