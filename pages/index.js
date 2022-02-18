724cf43bb15eed0c6802c0023aeac804745f36db
Tejas Kumar <tejas@tejas.qa>
2022-02-18 16:39:02 +0100

    Fix link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   function randomIntFromInterval(min, max) {
@@ -34,19 +34,13 @@ export default function Home() {
         </section>
         <section className={styles.links}>
           <Link href="https://twitter.com/SrirajBura">
-            <button className={styles.link}>
-              <a>Twitter</a>
-            </button>
+            <a className={styles.link}>Twitter</a>
           </Link>
           <Link href="https://github.com/srirajbura16">
-            <button className={styles.link}>
-              <a>Github</a>
-            </button>
+            <a className={styles.link}>Github</a>
           </Link>
           <Link href="https://www.linkedin.com/in/srirajbura/">
-            <button className={styles.link}>
-              <a>Linkedin</a>
-            </button>
+            <a className={styles.link}>Linkedin</a>
           </Link>
         </section>
 
