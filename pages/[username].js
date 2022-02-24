dd8da8140ff3c215d26bfcc24ea9619226c2e2ea
Sriraj Bura <sriraj.bura16@gmail.com>
2022-02-24 12:30:25 -0500

    implemented dynamic user pages

---
+++ pages/[username].js
@@ -0,0 +1,88 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Url from "../components/Url";
+
+export default function Username({ data: { records } }) {
+  const { name, photo, description } = records[0].user;
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>John Doe</title>
+        <meta name="description" content="Sriraj Bura's Links" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <figure
+          className={styles.avatar}
+          style={{ backgroundImage: `url(${photo})` }}
+        />
+        <section>
+          <h2 className={styles.name}>{name}</h2>
+          <p className={styles.description}>{description}</p>
+        </section>
+        <section className={styles.links}>
+          {records.map((link) => {
+            return <Url link={link} key={link.id} />;
+          })}
+        </section>
+        <div className={styles.social_icons}></div>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  var bodyRaw = {
+    columns: ["url", "title", "user.*"],
+    filter: {
+      "user.username": `${params.username}`,
+    },
+  };
+  var requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.XATA_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bodyRaw),
+  };
+
+  const res = await fetch(
+    "https://open-mpqb7l.xata.sh/db/Linktree:main/tables/Links/query",
+    requestOptions
+  );
+
+  const data = await res.json();
+
+  return {
+    props: { data },
+
+    revalidate: 10,
+  };
+}
+
+export async function getStaticPaths() {
+  var bodyRaw = { columns: ["username"] };
+  var requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.XATA_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bodyRaw),
+  };
+
+  const res = await fetch(
+    "https://open-mpqb7l.xata.sh/db/Linktree:main/tables/Users/query",
+    requestOptions
+  );
+
+  const users = await res.json();
+  const paths = users.records.map((user) => ({
+    params: { username: user.username },
+  }));
+
+  return { paths, fallback: true };
+}
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home({ data: { records } }) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  console.log(records);
+
   const img = `https://picsum.photos/${randomIntFromInterval(
     200,
     800
@@ -15,55 +17,15 @@ export default function Home({ data: { records } }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>John Doe</title>
+        <title>Induit</title>
         <meta name="description" content="Sriraj Bura's Links" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <figure
-          className={styles.avatar}
-          style={{ backgroundImage: `url(${img})` }}
-        />
-        <section>
-          <h2 className={styles.name}>John Doe</h2>
-          <p className={styles.description}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde,
-            libero.
-          </p>
-        </section>
-        <section className={styles.links}>
-          {records.map((link) => {
-            return <Url link={link} key={link.id} />;
-          })}
-        </section>
-        <div className={styles.social_icons}></div>
+        <Link href="/johndoe">johndoe</Link>
+        <Link href="/janedoe">janedoe</Link>
       </main>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  var bodyRaw = {
-    columns: ["url", "title"],
-  };
-  var requestOptions = {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.XATA_API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(bodyRaw),
-  };
-
-  const res = await fetch(
-    "https://open-mpqb7l.xata.sh/db/Linktree:main/tables/Links/query",
-    requestOptions
-  );
-
-  const data = await res.json();
-
-  return {
-    props: { data },
-  };
 }
