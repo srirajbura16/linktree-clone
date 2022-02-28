7fa0e76242bc9d5069613d0be893e2c9778d253f
Sriraj Bura <sriraj.bura16@gmail.com>
2022-02-28 17:45:27 -0500

    added boiler setting pages

---
+++ pages/settings/account.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Account() {
+  return (
+    <div>
+      here the user can change their personal information
+      <ul>
+        <li>Profile pic</li>
+        <li>Email</li>
+        <li>Name</li>
+        <li>Username</li>
+        <li>description</li>
+        <li>Password</li>
+      </ul>
+    </div>
+  );
+}
---
+++ pages/settings/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function index() {
+  return <div>Links pointing to account and links page.</div>;
+}
